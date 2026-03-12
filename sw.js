760ae8664fd021af29211cf0ac70bd752720f569
Azuzez5 <phamanhkhoa1708ltv@gmail.com>
2026-03-12 17:34:33 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-
-const CACHE_NAME="audio-player-v1"
+const CACHE_NAME="audio-player-v2"
 
 const ASSETS=[
 "/",
@@ -10,17 +9,36 @@ const ASSETS=[
 ]
 
 self.addEventListener("install",e=>{
+
+self.skipWaiting()
+
 e.waitUntil(
 caches.open(CACHE_NAME).then(cache=>{
 return cache.addAll(ASSETS)
 })
 )
+
+})
+
+self.addEventListener("activate",e=>{
+
+e.waitUntil(
+caches.keys().then(keys=>{
+return Promise.all(
+keys.filter(k=>k!==CACHE_NAME)
+.map(k=>caches.delete(k))
+)
+})
+)
+
 })
 
 self.addEventListener("fetch",e=>{
+
 e.respondWith(
 caches.match(e.request).then(res=>{
 return res || fetch(e.request)
 })
 )
+
 })
